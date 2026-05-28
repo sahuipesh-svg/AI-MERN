6d6e71c0111caa2a261ef868431dc95047d62882
sahuip <ipeshkumarsahu2020@gmail.com>
2026-05-28 23:09:58 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json())
 
 app.use(cookieParser())
 app.use(cors({
-   origin:"http://localhost:5173",
+   origin:"https://webcraft-frontend.onrender.com",
    credentials:true
 }))
 app.use("/api/auth",authRouter)
